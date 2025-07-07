3c7471fd5a3ae8553048913cfe02a8700d84844b
I.M KING <mirukaiano@gmail.com>
2025-07-07 12:42:02 +0300

    Update Hyprcandy_installer.sh

--- Hyprcandy_installer.sh
+++ Hyprcandy_installer.sh
@@ -2627,10 +2627,10 @@ bind = $mainMod CTRL, minus, exec, ~/.config/hyprcandy/hooks/hyprland_rounding_d
 
 #### Visual presets ####
 
-bind = $mainMod, F1, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh minimal
-bind = $mainMod, F2, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh balanced
-bind = $mainMod, F3, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh spacious
-bind = $mainMod, F4, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh zero
+bind = SHIFT, 1, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh minimal
+bind = SHIFT, 2, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh balanced
+bind = SHIFT, 3, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh spacious
+bind = SHIFT, 4, exec, ~/.config/hyprcandy/hooks/hyprland_gap_presets.sh zero
 
 #### Status display ####
 
